ff47828e8f643228425150299e58a131fb8b9100
Iuliia <markelova.uv@gmail.com>
2024-07-12 12:21:17 +0300

    removed empty line

--- src/main/java/cy/markelova/government/_main/_Main.java
+++ src/main/java/cy/markelova/government/_main/_Main.java
@@ -6,7 +6,6 @@ import cy.markelova.government.service.MenuActions;
 public class _Main {
 
     public static void main(String[] args) {
-
         Government government = Government.getInstance();
         MenuActions menu = new MenuActions();
         menu.runMenu();
