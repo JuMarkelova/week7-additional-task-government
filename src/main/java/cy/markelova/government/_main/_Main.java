8eaaf6cb6ec214edb930255c78bab25213a894cc
Iuliia <markelova.uv@gmail.com>
2024-07-12 12:23:10 +0300

    removed explicit government call

--- src/main/java/cy/markelova/government/_main/_Main.java
+++ src/main/java/cy/markelova/government/_main/_Main.java
@@ -1,12 +1,10 @@
 package cy.markelova.government._main;
 
-import cy.markelova.government.entity.Government;
 import cy.markelova.government.service.MenuActions;
 
 public class _Main {
 
     public static void main(String[] args) {
-        Government government = Government.getInstance();
         MenuActions menu = new MenuActions();
         menu.runMenu();
     }
